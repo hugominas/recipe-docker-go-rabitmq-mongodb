f3efb06f8c485f2083835ac0de48d1c9f8f8f978
hugo <hugo.mineiro.r@gmail.com>
2017-05-04 20:46:55 +0100

    added initial structure

---
+++ api/lib/database/init.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func Init() (string){
+	session, err := mgo.Dial("apiuser:1234@mongodb/apiData")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("apiData").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result.Phone
+}
